bde9daa6d281b53ee3182e0bd6eb15b82b2afa6b
Lucas Souza <ls79297@gmail.com>
2019-11-13 00:31:13 -0300

    Redux saga configurado

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigtório'),
+  password: Yup.string()
+    .min(6, 'No minímo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
-  return <h1>Register</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
